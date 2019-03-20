16726f58a1e886cc7ca75a7ea94e77da1c00c024
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-03-20 15:33:17 -0400

    Add needle plot.

---
+++ components/needle_plot.py
@@ -0,0 +1,28 @@
+import dash_html_components as html
+import dash_bio as dashbio
+import json
+import urllib.request as urlreq
+import base64
+
+data = urlreq.urlopen("https://raw.githubusercontent.com/plotly/dash-bio/master/tests/dashbio_demos/sample_data/needle_PIK3CA.json").read()
+mdata = json.loads(data)
+
+component = dashbio.NeedlePlot(
+  id='my-dashbio-needleplot',
+  mutationData=mdata
+)
+
+component_image = html.Img(
+    src='data:image/png;base64,{}'.format(
+        base64.b64encode(
+            open(
+                './images/pic_needle_plot.png', 'rb'
+            ).read()
+        ).decode()
+    ),
+    style={'width': '700px'}
+)
+
+
+def callbacks(app):
+    return
